9dce9f5c5b11c72ef5038e246bfc7607c0925889
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-03-05 01:04:26 +0600

    card made

--- client/src/components/EventPageComp/EventPageComp.js
+++ client/src/components/EventPageComp/EventPageComp.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import "./EventPageComp.css";
 
 export default function EventPageComp() {
   const [event, setevent] = useState([]);
@@ -26,19 +27,16 @@ export default function EventPageComp() {
         <div className="row mt-5">
           {event.map((item) => {
             return (
-              <div className="col">
+              <div className="col mt-3">
                 <div class="card">
                   <img
-                    class="card-img-top w-100 h-100"
+                    class="card-img-top"
                     src={item.event_image}
                     alt="Card image cap"
                   />
                   <div class="card-body">
                     <h5 class="card-title">{item.event_name}</h5>
-                    <p class="card-text">
-                      {item.event_description}
-                      DJHDJSHJDHSJHDJHSDJHHHHDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDHHHHHHHHHHHHHDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-                    </p>
+                    <p class="card-text">{item.event_description}</p>
                   </div>
                 </div>
               </div>

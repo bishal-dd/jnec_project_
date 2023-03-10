f824cb936bdfbd51e9817637bd19719b3c087741
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-03-09 22:08:24 -0800

    images added

--- client/src/components/CarouselComp/CarouselComp.js
+++ client/src/components/CarouselComp/CarouselComp.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import "./carouselcomp.css";
+import Admin from "../../assets/Admin block.jpeg"
+import Campus from "../../assets/JNEC campus.jpeg"
 
 export default function CarouselComp() {
   const images = [
     {
-      url: "https://image-processor-storage.s3.us-west-2.amazonaws.com/images/3cf61c1011912a2173ea4dfa260f1108/halo-of-neon-ring-illuminated-in-the-stunning-landscape-of-yosemite.jpg",
-      caption: "Some Text should be put here",
+      url: Admin,
+      caption: ""
     },
     {
-      url: "https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?b=1&k=20&m=517188688&s=612x612&w=0&h=x8h70-SXuizg3dcqN4oVe9idppdt8FUVeBFemfaMU7w=",
-      caption: "Some Text should be put here",
+      url: Campus,
+      caption: "",
     },
     {
       url: "https://img.freepik.com/free-photo/abstract-flowing-neon-wave-background_53876-101942.jpg",

05dad4b344a18ba84aeb72eaf932862ec685a717
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-03-11 16:15:30 +0600

    switched pages

--- client/src/components/ProjectmemberComp/ProjectmemberComp.js
+++ client/src/components/ProjectmemberComp/ProjectmemberComp.js
@@ -3,65 +3,70 @@ import "./projectmembercomp.css";
 
 export default function ProjectmemberComp() {
   return (
-    <div className="container">
-      <div>
-        <h3 className="mt-5">Jigme Namgyel Engineering College </h3>
-        <p class="project-objectives mt-3">
-          JNEC is one of the constituent colleges of the Royal University of
-          Bhutan (RUB) located in Dewathang. The College started with the
-          infrastructure development in 1974 coinciding with the 3rd Five Year
-          Development Plan of the country and offering Diploma programmes in
-          Civil, Electrical and Mechanical Engineering. The College currently
-          offers ten academic programmes; seven Diploma and three Bachelor of
-          Engineering. The College continues to explore, plan and offer academic
-          programmes based on the socio‐economic needs of the country. As a
-          premier college, several works are also being carried out to support
-          community development. The college has been accredited by the Bhutan
-          Accreditation Council and has been awarded Grade – A.
-        </p>
+    <div className="container mt-5 ">
+      <h2 className="text-center mb-5">Project Members</h2>
+      <div className="row  d-flex justify-content-center ">
+        <div className="col text-center">
+          <img
+            src="https://res.cloudinary.com/dnmtsuwhc/image/upload/v1678527860/loxb7isez3u80y9vdbwj.png"
+            alt="metting"
+            className="w-50"
+          />
+        </div>
       </div>
-      <div>
-        <h3 className="mt-5">University of Innsbruck </h3>
-        <p class="project-objectives mt-3">
-          University of Innsbruck has in totals 28000 students and 16 faculties.
-          The Institute for Structural Engineering and Material Sciences belongs
-          to the faculty of Engineering Sciences. It consists of 6 Units of
-          which the Units Energy Efficient Buildings and Timber Engineering are
-          taking part in the project. The Unit of Energy Efficient Buildings
-          (EEB) with combines two directions. Building physics deals on the one
-          hand with thermal and moisture transport as well as acoustics of
-          building construction, natural and artificial lighting. Additionally,
-          the topics of HVAC (Heating, Ventilation & Air Conditioning) systems,
-          renewable energy integration (solar, biomass, heat pumps), efficient
-          energy systems for buildings and city quarters as well as spatial
-          energy planning are covered. The core competence of the Unit of Timber
-          Engineering is practice‐oriented research in the field of timber
-          engineering. The researchers at the Unit of Timber Engineering are
-          trained and experienced structural engineers or architects, conducting
-          basic and applied research on wood as a building material, new wood
-          products and state‐of‐the‐art fastener technology. The Unit of Timber
-          Engineering has excellent possibilities to produce high‐quality
-          specimens for experimental research with state‐of‐the‐art CNC
-          technology. It also has access to testing facilities to realize
-          experiments for multiple scales of basic and applied timber
-          engineering research. An additional research focus is sound and fire
-          protection. The Department of Subject‐Specific Education unites the
-          subject‐specific education of future teachers. The section Science,
-          Geography, Computer Science, and Mathematics Education will be
-          involved in this project for the STEM education training. UIBK was
-          active in the following projects in North‐South cooperation: HEBA:
-          High level rEnewabBle and energy efficiency mAster courses;
-          EGREEN–Development of Environmental Engineering and injection of
-          climate change concept for Undergraduate curriculum :EU experience for
-          Jordan & Syria; CIMCEB: Curricula Development of Interdisciplinary
-          Master Courses in Energy Efficient Building Design in Nepal & Bhutan;
-          Master programmes on Renewable Energy and Energy Efficiency in
-          Buildings in Central Asia & Russia; Development and implementation of
-          a Master Course "Energy Management" in three Libyan Universities;
-          MAPEC Curriculum development for Master Program of Environmental
-          engineering and Climate change; Sustainable comfort through building
-          optimization and solar cooling in India.
-        </p>
+
+      <div className="row mt-4" id="aboutus_row">
+        <div className="col-md-6">
+          <h4>JNEC</h4>
+          <ol className="">
+            <li>
+              <strong>Dr Tshewang Lhendup</strong> – Project Adviser
+            </li>
+            <li>
+              <strong>Mr Samten Lhendup</strong> – Project Manager/Coordinator
+            </li>
+            <li>
+              <strong>Mr Rigden Yoezer Tenzin</strong> – Project
+              Member/Secretary
+            </li>
+            <li>
+              <strong>Ms Wangmo</strong> – Member
+            </li>
+            <li>
+              <strong>Mr Lobzang Dorji</strong> – Member
+            </li>
+            <li>
+              <strong>Ms Thinley Wangmo</strong> – Member
+            </li>
+            <li>
+              <strong>Mr Phurba Tamang</strong> – Member
+            </li>
+            <li>
+              <strong>Ms Yeshey Dema</strong> – Project Accountant
+            </li>
+          </ol>
+        </div>
+
+        <div className="col-md-6">
+          <h4>UIBK</h4>
+          <ol className="list">
+            <li>
+              <strong>Prof. Wolfgang Streicher</strong> – Partner Coordinator
+            </li>
+            <li>
+              <strong>Prof. Suzanne Kapelari</strong> – Member
+            </li>
+            <li>
+              <strong>Prof. Anton Kraler</strong> – Member
+            </li>
+            <li>
+              <strong>Prof. Roland Maderebner</strong> – Member
+            </li>
+            <li>
+              <strong>Dr Martin Hauer</strong> - Member
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
